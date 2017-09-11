7fadf4f78e1ad5323a8888f1d23c23a201813eb1
Jeffrey Witt <jcwitt@JeffsComputer.local>
2017-09-11 09:20:14 -0400

    added dummy crawl file for presentations

---
+++ bin/dm-rdf.sh
@@ -0,0 +1,485 @@
+# /bin/bash
+sleep 1
+echo "Creating projectfiles metadata"
+sleep 1
+echo "getting projectfiles version for /Users/jcwitt/Projects/scta/scta-projectfiles/"
+sleep 1
+echo "2c2afce"
+sleep 1
+echo "Creating metadata assertion for ada-projectdata"
+sleep 1
+echo "Creating metadata assertion for adc-projectdata"
+sleep 1
+echo "Creating metadata assertion for adh-projectdata"
+sleep 1
+echo "Creating metadata assertion for ahsh-projectdata"
+sleep 1
+echo "Creating metadata assertion for als-projectdata"
+sleep 1
+echo "Creating metadata assertion for am-projectdata"
+sleep 1
+echo "Creating metadata assertion for anon1-projectdata"
+sleep 1
+echo "Creating metadata assertion for anon2-projectdata"
+sleep 1
+echo "Creating metadata assertion for atv-projectdata"
+sleep 1
+echo "Creating metadata assertion for atvda-projectdata"
+sleep 1
+echo "Creating metadata assertion for aw-projectdata"
+sleep 1
+echo "Creating metadata assertion for awa-projectdata"
+sleep 1
+echo "Creating metadata assertion for awls-projectdata"
+sleep 1
+echo "Creating metadata assertion for bb-projectdata"
+sleep 1
+echo "Creating metadata assertion for cice-liberextra"
+sleep 1
+echo "Creating metadata assertion for cl-0139"
+sleep 1
+echo "Creating metadata assertion for cl-0140"
+sleep 1
+echo "Creating metadata assertion for cl-0141"
+sleep 1
+echo "Creating metadata assertion for cl-0142"
+sleep 1
+echo "Creating metadata assertion for cl-0143"
+sleep 1
+echo "Creating metadata assertion for cl-0144"
+sleep 1
+echo "Creating metadata assertion for cl-0145"
+sleep 1
+echo "Creating metadata assertion for cl-0146"
+sleep 1
+echo "Creating metadata assertion for cl-0147"
+sleep 1
+echo "Creating metadata assertion for da-105-monsterbekquaestiones-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-116-anonymusquaestiones-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-199-aegidiusexpositio-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-236-hispanussententia-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-238-sigerbrabantquaestiones-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-242-favershamquaestiones-projectfile"
+sleep 1
+echo "Creating metadata assertion for da-255-anonymusquaestiones-projectfile"
+sleep 1
+echo "Creating metadata assertion for da-263-aegidiustractatus-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-28-jandundequaestiones-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-3-hedonensistractatus-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-318-anonymusquaestiones-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-327-anonymusquaestiones-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-328-anonymusquaestiones-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-348-anonymusquaestiones-projectfile"
+sleep 1
+echo "Creating metadata assertion for da-350-peckhamquaestiones-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-352-anonymusquaestiones-projectfile"
+sleep 1
+echo "Creating metadata assertion for da-353-anonymusquaestiones-projectfile"
+sleep 1
+echo "Creating metadata assertion for da-49-dinsdalequaestiones-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-5-anonymusexpositio-projectfile"
+sleep 1
+echo "Creating metadata assertion for da-57-alexanderexpositio-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-65-anonymusquaestions-projectfile"
+sleep 1
+echo "Creating metadata assertion for da-69-anonymus-projectdata"
+sleep 1
+echo "Creating metadata assertion for da-9-anonymusquaestiones-projectdata"
+sleep 1
+echo "Creating metadata assertion for ddb-projectdata"
+sleep 1
+echo "Creating metadata assertion for euclidelements"
+sleep 1
+echo "Creating metadata assertion for ftdp-projectdata"
+sleep 1
+echo "Creating metadata assertion for gb-projectdata"
+sleep 1
+echo "Creating metadata assertion for gd-gratiandecretum"
+sleep 1
+echo "Creating metadata assertion for gdr-projectdata"
+sleep 1
+echo "Creating metadata assertion for gds-projectdata"
+sleep 1
+echo "Creating metadata assertion for gr-projectdata"
+sleep 1
+echo "Creating metadata assertion for hduv-projectdata"
+sleep 1
+echo "Creating metadata assertion for hn-projectdata"
+sleep 1
+echo "Creating metadata assertion for holcotquodlibeta"
+sleep 1
+echo "Creating metadata assertion for hw-projectdata"
+sleep 1
+echo "Creating metadata assertion for ja-projectdata"
+sleep 1
+echo "Creating metadata assertion for jaldh-projectdata"
+sleep 1
+echo "Creating metadata assertion for jdso-projectdata"
+sleep 1
+echo "Creating metadata assertion for jh-projectdata"
+sleep 1
+echo "Creating metadata assertion for jhb-projectdata"
+sleep 1
+echo "Creating metadata assertion for jm-projectdata"
+sleep 1
+echo "Creating metadata assertion for lm-projectdata"
+sleep 1
+echo "Creating metadata assertion for nddm-projectdata"
+sleep 1
+echo "Creating metadata assertion for pam-projectdata"
+sleep 1
+echo "Creating metadata assertion for pda-projectdata"
+sleep 1
+echo "Creating metadata assertion for pdah-dionysiusangelica"
+sleep 1
+echo "Creating metadata assertion for pdeh-dionysiusecclesiastica"
+sleep 1
+echo "Creating metadata assertion for pdma-projectdata"
+sleep 1
+echo "Creating metadata assertion for pdt-projectdata"
+sleep 1
+echo "Creating metadata assertion for pg-projectdata"
+sleep 1
+echo "Creating metadata assertion for phah-projectdata"
+sleep 1
+echo "Creating metadata assertion for pheh-projectdata"
+sleep 1
+echo "Creating metadata assertion for phsl-projectdata"
+sleep 1
+echo "Creating metadata assertion for phsyn-projectdata"
+sleep 1
+echo "Creating metadata assertion for pjo-projectdata"
+sleep 1
+echo "Creating metadata assertion for pl-projectdata"
+sleep 1
+echo "Creating metadata assertion for pp-projectdata"
+sleep 1
+echo "Creating metadata assertion for ppr-projectdata"
+sleep 1
+echo "Creating metadata assertion for rdm-projectdata"
+sleep 1
+echo "Creating metadata assertion for rfr-projectdata"
+sleep 1
+echo "Creating metadata assertion for rh-projectdata"
+sleep 1
+echo "Creating metadata assertion for rhwis-projectdata"
+sleep 1
+echo "Creating metadata assertion for ta-projectdata"
+sleep 1
+echo "Creating metadata assertion for tda-projectdata"
+sleep 1
+echo "Creating metadata assertion for vocabularius"
+sleep 1
+echo "Creating metadata assertion for wc-projectdata"
+sleep 1
+echo "Creating metadata assertion for wcl-projectdata"
+sleep 1
+echo "Creating metadata assertion for wcr-projectdata"
+sleep 1
+echo "Creating metadata assertion for wdr-projectdata"
+sleep 1
+echo "Creating metadata assertion for wo-projectdata"
+sleep 1
+echo "Creating metadata assertion for wv-projectdata"
+sleep 1
+echo "Projectfiles meta data created for all projectfiles"
+sleep 1
+echo "Begin codices meta data creation"
+sleep 1
+echo "Creating metadata assertion for Basel1557"
+sleep 1
+echo "Creating metadata assertion for Basel1586"
+sleep 1
+echo "Creating metadata assertion for Friedberg1879a"
+sleep 1
+echo "Creating metadata assertion for Friedberg1879b"
+sleep 1
+echo "Creating metadata assertion for Paris1490"
+sleep 1
+echo "Creating metadata assertion for Paris1514"
+sleep 1
+echo "Creating metadata assertion for Paris1526"
+sleep 1
+echo "Creating metadata assertion for Rome1582"
+sleep 1
+echo "Creating metadata assertion for avignon311"
+sleep 1
+echo "Creating metadata assertion for bal311"
+sleep 1
+echo "Creating metadata assertion for balliol246"
+sleep 1
+echo "Creating metadata assertion for baselAVI22"
+sleep 1
+echo "Creating metadata assertion for bda446"
+sleep 1
+echo "Creating metadata assertion for bnf"
+sleep 1
+echo "Creating metadata assertion for bnf14173"
+sleep 1
+echo "Creating metadata assertion for bnf14454"
+sleep 1
+echo "Creating metadata assertion for bnf14514"
+sleep 1
+echo "Creating metadata assertion for bnf14556"
+sleep 1
+echo "Creating metadata assertion for bnf14563"
+sleep 1
+echo "Creating metadata assertion for bnf14564"
+sleep 1
+echo "Creating metadata assertion for bnf14576"
+sleep 1
+echo "Creating metadata assertion for bnf14577"
+sleep 1
+echo "Creating metadata assertion for bnf14716"
+sleep 1
+echo "Creating metadata assertion for bnf14899"
+sleep 1
+echo "Creating metadata assertion for bnf15331"
+sleep 1
+echo "Creating metadata assertion for bnf15363"
+sleep 1
+echo "Creating metadata assertion for bnf15369"
+sleep 1
+echo "Creating metadata assertion for bnf15705"
+sleep 1
+echo "Creating metadata assertion for bnf15853"
+sleep 1
+echo "Creating metadata assertion for bnf15884"
+sleep 1
+echo "Creating metadata assertion for bnf15885"
+sleep 1
+echo "Creating metadata assertion for bnf15889"
+sleep 1
+echo "Creating metadata assertion for bnf15890"
+sleep 1
+echo "Creating metadata assertion for bnf15891"
+sleep 1
+echo "Creating metadata assertion for bnf15893"
+sleep 1
+echo "Creating metadata assertion for bnf15894"
+sleep 1
+echo "Creating metadata assertion for bnf15898"
+sleep 1
+echo "Creating metadata assertion for bnf15908"
+sleep 1
+echo "Creating metadata assertion for bnf15909"
+sleep 1
+echo "Creating metadata assertion for bnf16126"
+sleep 1
+echo "Creating metadata assertion for bnf16406"
+sleep 1
+echo "Creating metadata assertion for bnf16611"
+sleep 1
+echo "Creating metadata assertion for bnf16791"
+sleep 1
+echo "Creating metadata assertion for bnf17483"
+sleep 1
+echo "Creating metadata assertion for bnf3061"
+sleep 1
+echo "Creating metadata assertion for bnf3063"
+sleep 1
+echo "Creating metadata assertion for bnf3068"
+sleep 1
+echo "Creating metadata assertion for bnf3073"
+sleep 1
+echo "Creating metadata assertion for bnf3114"
+sleep 1
+echo "Creating metadata assertion for bnf3423"
+sleep 1
+echo "Creating metadata assertion for bnf4311"
+sleep 1
+echo "Creating metadata assertion for bnf6433"
+sleep 1
+echo "Creating metadata assertion for bnf6657"
+sleep 1
+echo "Creating metadata assertion for bnf6675"
+sleep 1
+echo "Creating metadata assertion for bnf7521"
+sleep 1
+echo "Creating metadata assertion for bnf7798"
+sleep 1
+echo "Creating metadata assertion for bnf8567"
+sleep 1
+echo "Creating metadata assertion for borghlat359"
+sleep 1
+echo "Creating metadata assertion for clm14067"
+sleep 1
+echo "Creating metadata assertion for clm14150"
+sleep 1
+echo "Creating metadata assertion for clm14151"
+sleep 1
+echo "Creating metadata assertion for clm14172"
+sleep 1
+echo "Creating metadata assertion for clm14209"
+sleep 1
+echo "Creating metadata assertion for clm26711"
+sleep 1
+echo "Creating metadata assertion for clm27033"
+sleep 1
+echo "Creating metadata assertion for clm28212"
+sleep 1
+echo "Creating metadata assertion for clm2940"
+sleep 1
+echo "Creating metadata assertion for clm2958"
+sleep 1
+echo "Creating metadata assertion for clm8358"
+sleep 1
+echo "Creating metadata assertion for clm8718"
+sleep 1
+echo "Creating metadata assertion for erlang"
+sleep 1
+echo "Creating metadata assertion for fcc20"
+sleep 1
+echo "Creating metadata assertion for floji3"
+sleep 1
+echo "Creating metadata assertion for ghent567"
+sleep 1
+echo "Creating metadata assertion for gks1363"
+sleep 1
+echo "Creating metadata assertion for goncai342"
+sleep 1
+echo "Creating metadata assertion for goncaius281"
+sleep 1
+echo "Creating metadata assertion for harley3211"
+sleep 1
+echo "Creating metadata assertion for harv245"
+sleep 1
+echo "Creating metadata assertion for hopkinsMSB19"
+sleep 1
+echo "Creating metadata assertion for kra726"
+sleep 1
+echo "Creating metadata assertion for lon"
+sleep 1
+echo "Creating metadata assertion for maz"
+sleep 1
+echo "Creating metadata assertion for mun2805"
+sleep 1
+echo "Creating metadata assertion for oriel33"
+sleep 1
+echo "Creating metadata assertion for pal"
+sleep 1
+echo "Creating metadata assertion for paris"
+sleep 1
+echo "Creating metadata assertion for paris1512"
+sleep 1
+echo "Creating metadata assertion for penn"
+sleep 1
+echo "Creating metadata assertion for penn855"
+sleep 1
+echo "Creating metadata assertion for quaracchi1924"
+sleep 1
+echo "Creating metadata assertion for quaracchi1928"
+sleep 1
+echo "Creating metadata assertion for reims"
+sleep 1
+echo "Creating metadata assertion for sorb"
+sleep 1
+echo "Creating metadata assertion for sorb193"
+sleep 1
+echo "Creating metadata assertion for svict"
+sleep 1
+echo "Creating metadata assertion for urblat123"
+sleep 1
+echo "Creating metadata assertion for vat"
+sleep 1
+echo "Creating metadata assertion for vatlat1087"
+sleep 1
+echo "Creating metadata assertion for vatlat1379"
+sleep 1
+echo "Creating metadata assertion for vatlat680"
+sleep 1
+echo "Creating metadata assertion for vatlat681"
+sleep 1
+echo "Creating metadata assertion for vatlat682"
+sleep 1
+echo "Creating metadata assertion for vatlat683"
+sleep 1
+echo "Creating metadata assertion for vatlat684"
+sleep 1
+echo "Creating metadata assertion for vatlat685"
+sleep 1
+echo "Creating metadata assertion for vatlat687"
+sleep 1
+echo "Creating metadata assertion for vatlat688"
+sleep 1
+echo "Creating metadata assertion for vatlat689"
+sleep 1
+echo "Creating metadata assertion for vatlat690"
+sleep 1
+echo "Creating metadata assertion for vatlat693"
+sleep 1
+echo "Creating metadata assertion for vatlat701"
+sleep 1
+echo "Creating metadata assertion for vatlat955"
+sleep 1
+echo "Creating metadata assertion for venice1521"
+sleep 1
+echo "Creating metadata assertion for venz253"
+sleep 1
+echo "Creating metadata assertion for walt349"
+sleep 1
+echo "Creating metadata assertion for walt809"
+sleep 1
+echo "Creating metadata assertion for wellcome140"
+sleep 1
+echo "Creating metadata assertion for wellcome335"
+sleep 1
+echo "Creating metadata assertion for wellcome55"
+sleep 1
+echo "Creating metadata assertion for wettf15"
+sleep 1
+echo "Creating metadata assertion for zbsSII72"
+sleep 1
+echo "Codices Metadata completed"
+sleep 1
+echo "Begin top level archive collection creation"
+sleep 1
+echo "Begin quotation extraction from custom lists"
+sleep 1
+echo "creating anselm_quotationslist"
+sleep 1
+echo "creating aristotle_quotationslist"
+sleep 1
+echo "creating augustine_quotationslist"
+sleep 1
+echo "creating averroes_quotationslist"
+sleep 1
+echo "creating bible_quotations"
+sleep 1
+echo "creating canonlaw_quotations"
+sleep 1
+echo "creating dionysius_quotationslist"
+sleep 1
+echo "creating gregorythegreat_quotationlist"
+sleep 1
+echo "creating lombard_quotationslist"
+sleep 1
+echo "creating miscquotationslist"
+sleep 1
+echo "creating richardofstvictor_quotationlist"
+sleep 1
+echo "Begin Workcited Metadata extraction"
+sleep 1
+echo "Begin Nameslist Metadata extraction"
+sleep 1
+echo "Begin PersonGroupList Metadata extraction"
+sleep 1
+echo "Begin Subjectlist Metadata extraction"
+sleep 1
+echo "Begin Passive Relationships Metadata extraction"
+sleep 1
+echo "All finished"
