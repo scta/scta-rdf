850692210e00119f52fbb01ffbbaa69e036dbad4
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2018-07-13 20:19:33 -0400

    add shacommand to variables so that sha1sum could be used or other variations thereof on alpine system

--- bin2/config.rb
+++ bin2/config.rb
@@ -3,9 +3,11 @@ if ENV['DOCKER'] == "true"
   $fuseki="/home/fuseki/apache-jena-fuseki-2.3.1"
   $fuseki_builds="/home/scta-rdf/fuseki-builds"
   $textfilesbase="/home/scta-rdf/data/scta-texts/"
+  $shacommand="sha1sum"
 else
   $base="/Users/jcwitt/Projects/scta/scta-rdf"
   $fuseki="/Users/jcwitt/Applications/fuseki/apache-jena-fuseki-2.3.1"
   $fuseki_builds="/Users/jcwitt/Projects/scta/scta-builds"
   $textfilesbase="/Users/jcwitt/Projects/scta/scta-texts/"
+  $shacommand="shasum"
 end
